b09c388a4eaab5c1cca2d67ab36116f87365839f
boom_boy322 <evanifrimov@gmail.com>
2026-09-23 13:16:16 +0500

    fixed classes

--- backend/app.py
+++ backend/app.py
@@ -71,7 +71,7 @@ def log_request():
 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
-ECG_CLASSES = ['Normal', 'Atrial Fibrillation', 'Other', 'Noise', 'ST-elevation', 'ST-depression']
+ECG_CLASSES = ['CD', 'HYP', 'MI', 'NORM', 'STTC', 'NOISE']
 
 
 # ── Inference ─────────────────────────────────────────────────────────────────
