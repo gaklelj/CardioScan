2e2e1fd2c35533614a1e33e22802409309d725b8
boom_boy322 <evanifrimov@gmail.com>
2026-09-23 10:30:38 +0500

    cors fix 2

--- backend/app.py
+++ backend/app.py
@@ -56,6 +56,23 @@ app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'fallback-dev-key')
 CORS(app, origins='*', supports_credentials=False)
 
 @app.before_request
+def handle_options():
+    if request.method == 'OPTIONS':
+        from flask import Response as _Resp
+        r = _Resp()
+        r.headers['Access-Control-Allow-Origin'] = '*'
+        r.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        r.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return r
+
+@app.after_request
+def add_cors(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+@app.before_request
 def log_request():
     log.info('→ %s %s', request.method, request.path)
 
